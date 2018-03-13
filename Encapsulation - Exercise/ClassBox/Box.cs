f44718bb7c0285a9fff9946bf56f7010d746abe6
PenkaFileva <penka_80@abv.bg>
2018-03-13 23:40:49 +0200

    Add new files

---
+++ Encapsulation - Exercise/ClassBox/Box.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Box
+{
+    private double length;
+
+    public double Length
+    {
+        get { return length; }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{nameof(Length)} cannot be zero or negative.");
+            }
+            length = value;
+        }
+    }
+    private double width;
+
+    public double Width
+    {
+        get { return width; }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{nameof(Width)} cannot be zero or negative.");
+            }
+            width = value;
+        }
+    }
+    private double height;
+
+    public double Height
+    {
+        get { return height; }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{nameof(Height)} cannot be zero or negative.");
+            }
+            height = value;
+        }
+    }
+
+
+    public Box(double length, double width, double height)
+    {
+        this.Length = length;
+        this.Width = width;
+        this.Height = height;
+    }
+
+    public double GetSurfaceArea()
+    {
+        return 2 * this.Length * this.Height + 
+            2 * this.Length * this.Width + 2 * this.Height * this.Width;
+    }
+
+    public double GetLateralSurfaceArea()
+    {
+        return 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+    }
+
+    public double GetVolume()
+    {
+        return this.Length * this.Height * this.Width;
+    }
+}
---
+++ Encapsulation - Exercise/ClassBox/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            var boxParams = new double[3];
+            for (int i = 0; i < boxParams.Length; i++)
+            {
+                boxParams[i] = double.Parse(Console.ReadLine());
+            }
+
+            Box box = new Box(boxParams[0], boxParams[1], boxParams[2]);
+            var surfaceArea = box.GetSurfaceArea();
+            var lateralSurfaceArea = box.GetLateralSurfaceArea();
+            var volume = box.GetVolume();
+            Console.WriteLine($"Surface Area - {surfaceArea:f2}");
+            Console.WriteLine($"Lateral Surface Area - {lateralSurfaceArea:f2}");
+            Console.WriteLine($"Volume - {volume:f2}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+       
+    }
+}
