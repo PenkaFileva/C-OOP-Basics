d88543065c6500108bf2c1e6ba8bb89bacd5fd59
PenkaFileva <penka_80@abv.bg>
2018-06-27 20:59:33 +0300

    new files

---
+++ Defining Classes - Exercise/PokemonTrainer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<Trainer> trainers = new List<Trainer>();
+        string command;
+
+        while ((command = Console.ReadLine()) != "Tournament")
+        {
+            var tokens = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string trainerName = tokens[0];
+            string pokemonName = tokens[1];
+            string pokemonElement = tokens[2];
+            int pokemonHealth = int.Parse(tokens[3]);
+
+            //pokemon = new Pokemon(tokens[1], tokens[2], int.Parse(tokens[3]));          
+
+            if (!trainers.Any(t => t.Name == trainerName))
+            {
+                trainers.Add(new Trainer(trainerName));
+            }
+
+            Trainer currentTrainer = trainers.First(t => t.Name == trainerName);
+            Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+            currentTrainer.AddPokemon(pokemon);
+        }
+
+        while ((command = Console.ReadLine()) != "End")
+        {
+            foreach (var trainer in trainers)
+            {
+                if (trainer.Pokemons.Any(p => p.Element == command))
+                {
+                    trainer.IncreaseBadges();
+                }
+                else
+                {
+                    trainer.ReducePokemonsHealth();
+                    trainer.RemoveDead();
+                }
+            }
+        }
+
+        var currentTrainers = trainers.OrderByDescending(t => t.Badges).ToList();
+
+        foreach (var trainer in currentTrainers)
+        {
+            Console.WriteLine("{0} {1} {2}", trainer.Name, trainer.Badges, trainer.Pokemons.Count);
+        }
+    }
+}
---
+++ Defining Classes - Exercise/PokemonTrainer/Trainer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class Trainer
+{
+    //name, number of badges and a collection of pokemon
+    private string name;
+    private int badges;
+    private List<Pokemon> pokemons;
+
+    public Trainer(string name)
+    {
+        this.Name = name;
+        this.Badges = 0;
+        this.Pokemons = new List<Pokemon>();
+    }
+    public List<Pokemon> Pokemons
+    {
+        get { return pokemons; }
+        set { pokemons = value; }
+    }
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int Badges
+    {
+        get { return badges; }
+        set { badges = value; }
+    }
+
+    public void AddPokemon(Pokemon pokemon)
+    {
+        this.Pokemons.Add(pokemon);
+    }
+
+    public void IncreaseBadges()
+    {
+        this.Badges ++;
+    }
+
+    public void ReducePokemonsHealth()
+    {
+        foreach (var pokemon in this.Pokemons)
+        {
+            pokemon.ReduceHealth();
+        }
+    }
+
+    public void RemoveDead()
+    {
+        this.Pokemons = this.Pokemons
+            .Where(p => p.Health > 0)
+            .ToList();
+        //foreach (var pokemon in this.Pokemons)
+        //{
+        //    if (pokemon.Health <= 0)
+        //    {
+        //        this.Pokemons.Remove(pokemon);
+        //    }
+        //}
+    }
+    //public override string ToString()
+    //{
+    //    return $"{this.Name} {this.Badges} {this.Pokemons.Count}";
+    //}
+}
+
